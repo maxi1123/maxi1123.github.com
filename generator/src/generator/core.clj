a70cf2a52394679eef59d0269451d657e1bd5670
syc153 <syc153@gmail.com>
2013-02-19 16:59:40 +0000

    Added vote link to header

--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -27,7 +27,8 @@
        [:div#header.shadow ; header
         (link-to {:class "headeritem" :id "earthcraft"} "index.html" "home")
         (link-to {:class "headeritem"} "http://www.planetminecraft.com/server/factions-pvp-dedicated-earthcraft-pvp/" "planetminecraft")
-        (link-to {:class "headeritem"} "https://minestatus.net/63996-factions-pvp-spleef-shops-dedicated-earthcraft-pvp/vote" "minestatus")]
+        (link-to {:class "headeritem"} "https://minestatus.net/63996-factions-pvp-spleef-shops-dedicated-earthcraft-pvp/vote" "minestatus")
+        (link-to {:class "headeritem"} "http://minecraft-server-list.com/server/120955/vote/")]
        [:div#logo-container ; logo
         [:div#logo "Earthcraft"]
         [:div#description "play.earthcraftsurvival.com - Minecraft Bukkit Server Since 2012"]]
